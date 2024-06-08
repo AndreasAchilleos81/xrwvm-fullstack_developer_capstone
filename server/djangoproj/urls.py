fc9898d729b83a8448e09d96612ceda8a11d602a
AndreasAchilleos81 <andreas.p.achilleos@gmail.com>
2024-06-08 10:40:14 +0300

    Fixed urls.py was failing migrations

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -13,14 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
-from django.conf.urls.static import static
-from django.conf import settings
+# from django.contrib import admin
+# from django.urls import path, include
+# from django.views.generic import TemplateView
+# from django.conf.urls.static import static
+# from django.conf import settings
 
-urlpatterns = [
-    path('admin/', admin.site.urls),"""djangoproj URL Configuration
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+    
+"""djangoproj URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -47,8 +49,4 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name="Home.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-    path('djangoapp/', include('djangoapp.urls')),
-    path('', TemplateView.as_view(template_name="Home.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
