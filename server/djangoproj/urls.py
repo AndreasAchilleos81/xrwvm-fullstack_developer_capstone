089441e8e9d564c2c45ea27bf6795509a0edbd1a
AndreasAchilleos81 <andreas.p.achilleos@gmail.com>
2024-06-15 19:07:59 +0300

    get dealers view

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -21,7 +21,7 @@ Including another URLconf
 
 # urlpatterns = [
 #     path('admin/', admin.site.urls),
-    
+
 """djangoproj URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -37,12 +37,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
@@ -51,4 +51,5 @@ urlpatterns = [
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('login/', TemplateView.as_view(template_name="index.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
