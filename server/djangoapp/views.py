349b954a41130fdc2a36362a0c7ce4a9ba037933
AndreasAchilleos81 <andreas.p.achilleos@gmail.com>
2024-06-22 15:55:00 +0300

    local flake8 changes

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -76,10 +76,10 @@ def registration(request):
     # If it is a new user
     if not username_exist:
         # Create user in auth_user table
-        user = User.objects.create_user(username=username, 
-                                        first_name=first_name, 
-                                        last_name=last_name, 
-                                        password=password, 
+        user = User.objects.create_user(username=username,
+                                        first_name=first_name,
+                                        last_name=last_name,
+                                        password=password,
                                         email=email)
         # Login the user and redirect to list page
         login(request, user)
@@ -90,7 +90,9 @@ def registration(request):
         return JsonResponse(data)
 
 
-# Update the `get_dealerships` view to render the index page with Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
+# Update the `get_dealerships` view to render the index page with
+# Update the `get_dealerships` render list of dealerships all by
+# default, particular state if state is passed
 def get_dealerships(request, state="All"):
     if (state == "All"):
         endpoint = "/fetchDealers"
